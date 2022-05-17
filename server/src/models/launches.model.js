026b6877442261c3518a4968c647bb8f617456e7
Gino Ochoa <gochoa1088@gmail.com>
2022-05-17 16:44:10 -0700

    POST launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS2",
   launchDate: new Date("December 27, 2030"),
   destination: "Kepler-442 b",
-  customer: ["NASA", "ZTM"],
+  customers: ["NASA", "ZTM"],
   upcoming: true,
   success: true,
 };
@@ -17,6 +19,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customers: ["ZTM", "NASA"],
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
