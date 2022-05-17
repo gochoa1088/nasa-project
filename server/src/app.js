2ae258c27534a6b2fe45807b1a1d00f9226500dd
Gino Ochoa <gochoa1088@gmail.com>
2022-05-17 11:31:15 -0700

    applying cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors"); // cross origin request
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
