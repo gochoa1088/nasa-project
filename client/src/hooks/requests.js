4d057d321be5b0cb44202a4f52aab2862b577dca
Gino <40076376+gochoa1088@users.noreply.github.com>
2022-08-05 20:40:15 -0700

    requests cleanup

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,12 +1,10 @@
 const API_URL = "http://localhost:8000/v1";
 
-// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
 }
 
-// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches = await response.json();
@@ -15,7 +13,6 @@ async function httpGetLaunches() {
   });
 }
 
-// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
@@ -32,7 +29,6 @@ async function httpSubmitLaunch(launch) {
   }
 }
 
-// Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
